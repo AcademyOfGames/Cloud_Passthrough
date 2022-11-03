a917851a27ddf04c633bee7165ccb5f9f7424881
Paul Harding Stewart <paulhardingstewart@gmail.com>
2022-11-03 10:05:50 -0700

    Polishing transition between worlds 1 and 2

--- Assets/DissolveControl.cs
+++ Assets/DissolveControl.cs
@@ -10,7 +10,15 @@ public class DissolveControl : MonoBehaviour
     private Transform objPos;
     private Material m;
 
-    private float completeDissolve = 2000f;
+    public GameObject groundCollider1;
+    public GameObject groundCollider2;
+    public GameObject brickWalls;
+
+    public GameObject deer;
+    public ParticleSystem mist;
+
+
+    private float completeDissolve = 250f;
     void Start()
     {
         m = GetComponent<MeshRenderer>().material;
@@ -29,18 +37,25 @@ public class DissolveControl : MonoBehaviour
         
         while (transition <= 1)
         {
-            transition += Time.deltaTime * .08f;
+            transition += Time.deltaTime * .24f;
             newDissolve = Mathf.Lerp(0, completeDissolve,transition);
             m.SetFloat("dissolve", newDissolve);
             yield return null;
         }
+        deer.SetActive(false);
+        groundCollider1.SetActive(false);
+        brickWalls.SetActive(false);
+        groundCollider2.SetActive(false);
+        mist.gravityModifier = .5f;
+
+        Invoke("hidePassthroughObjects", 1);
         SceneManager.LoadSceneAsync("GrassyHlls",LoadSceneMode.Additive);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(.1f);
         transition = 0;
         float transparency = m.GetFloat("transparency");
         while (transition <= 1)
         {
-            transition += Time.deltaTime * .005f;
+            transition += Time.deltaTime * .01f;
             transparency = Mathf.Lerp(transparency, 0,transition);
             m.SetFloat("transparency", transparency);
             yield return null;
@@ -48,7 +63,17 @@ public class DissolveControl : MonoBehaviour
 
 
     }
- 
+
+    void hidePassthroughObjects()
+    {
+        mist.gameObject.SetActive(false);
+        foreach(GameObject b in GameObject.FindGameObjectsWithTag("Brick"))
+        {
+            b.SetActive(false);
+        }
+    }
+
+
     void Update(){
     }
 }
