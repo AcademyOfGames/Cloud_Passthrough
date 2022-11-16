216f06f01136f2bc6e2e1874b69f2c5972602086
JG <99544977+AcademyOfGames@users.noreply.github.com>
2022-11-16 14:35:09 -0600

    optimizing scene for app lab submission

--- Assets/BrickLogic.cs
+++ Assets/BrickLogic.cs
@@ -14,74 +14,79 @@ public class BrickLogic : MonoBehaviour
     private static bool brickCrashSound;
 
     private float timePassed = 0;
+
+    public void CrashSound()
+    {
+        FindObjectOfType<TreeBehavior>().CrashThroughBricks();
+    }
     // Start is called before the first frame update
-    void Start()
-    {
-        renderer = GetComponent<MeshRenderer>();
-        renderer.enabled = false;
-
-        Invoke(nameof(ActivateBrick),7.7f);
-    }
-
-    void ActivateBrick()
-    {
-        _activated = true;
-
-    }
-    private void OnCollisionEnter(Collision other)
-    {
-        if (_activated )
-        {
-           // StartCoroutine(nameof(TurnColliderOffAndOn));
-
-            if (!brickCrashSound)
-            {
-                FindObjectOfType<TreeBehavior>().CrashThroughBricks();
-                brickCrashSound = true;
-            }
-            renderer.enabled = true;
-        }
-
-        if (other.gameObject.CompareTag("floor") ||other.gameObject.CompareTag("Brick") )
-        {
-            _onFloor = true;
-        }
-    }
-
-    private void OnCollisionExit(Collision other)
-    {
-        if (other.gameObject.CompareTag("floor")||other.gameObject.CompareTag("Brick") )
-        {
-            _onFloor = false;
-        }    
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (transform.position.y > 5)
-        {
-            GetComponent<BoxCollider>().enabled = false;
-        }
-        else
-        {
-            GetComponent<BoxCollider>().enabled = true;
-
-        }
-    }
-
-    IEnumerator TurnColliderOffAndOn()
-    {
-        yield return new WaitForSeconds(Random.Range(8f,10f));
-
-        while (transform.position.y > 5)
-        {
-            GetComponent<BoxCollider>().enabled = false;
-            yield return new WaitForSeconds(.6f); 
-            GetComponent<BoxCollider>().enabled = true; 
-            yield return new WaitForSeconds(Random.Range(.6f,1.8f));
-        }
-        
-    }
+    /* void Start()
+     {
+         renderer = GetComponent<MeshRenderer>();
+         renderer.enabled = false;
+ 
+         Invoke(nameof(ActivateBrick),7.7f);
+     }
+ 
+     void ActivateBrick()
+     {
+         _activated = true;
+ 
+     }
+     private void OnCollisionEnter(Collision other)
+     {
+         if (_activated )
+         {
+            // StartCoroutine(nameof(TurnColliderOffAndOn));
+ 
+             if (!brickCrashSound)
+             {
+                 FindObjectOfType<TreeBehavior>().CrashThroughBricks();
+                 brickCrashSound = true;
+             }
+             renderer.enabled = true;
+         }
+ 
+         if (other.gameObject.CompareTag("floor") ||other.gameObject.CompareTag("Brick") )
+         {
+             _onFloor = true;
+         }
+     }
+ 
+     private void OnCollisionExit(Collision other)
+     {
+         if (other.gameObject.CompareTag("floor")||other.gameObject.CompareTag("Brick") )
+         {
+             _onFloor = false;
+         }    
+     }
+ 
+ 
+     // Update is called once per frame
+     void Update()
+     {
+         if (transform.position.y > 5)
+         {
+             GetComponent<BoxCollider>().enabled = false;
+         }
+         else
+         {
+             GetComponent<BoxCollider>().enabled = true;
+ 
+         }
+     }
+ 
+     IEnumerator TurnColliderOffAndOn()
+     {
+         yield return new WaitForSeconds(Random.Range(8f,10f));
+ 
+         while (transform.position.y > 5)
+         {
+             GetComponent<BoxCollider>().enabled = false;
+             yield return new WaitForSeconds(.6f); 
+             GetComponent<BoxCollider>().enabled = true; 
+             yield return new WaitForSeconds(Random.Range(.6f,1.8f));
+         }
+         
+     }*/
 }
