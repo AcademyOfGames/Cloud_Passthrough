a3269cd77b2dc6527415835cbe5d8caa97ff0a1f
JG <99544977+AcademyOfGames@users.noreply.github.com>
2022-12-02 13:51:21 -0600

    adjusting bee animation, adding seed materials

---
+++ Assets/SeedBehavior.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedBehavior : MonoBehaviour
+{
+    private SeedGrabbableBehavior _grabInfo;
+    public Transform handPos;
+    public GameObject seedParticles;
+
+    public GameObject flowers;
+
+    private bool grabbed;
+    private Transform snapPos;
+    void Awake()
+    {
+        _grabInfo = GetComponent<SeedGrabbableBehavior>();
+    }
+    private void OnEnable() {
+        _grabInfo.OnGrabBegin.AddListener(IsGrabbed);
+        _grabInfo.OnGrabEnd.AddListener(IsReleased);
+    }
+ 
+    private void OnDisable() {
+        _grabInfo.OnGrabBegin.RemoveListener(IsGrabbed);
+        _grabInfo.OnGrabEnd.RemoveListener(IsReleased);
+    }
+
+    void IsGrabbed()
+    {
+        GetComponent<MeshRenderer>().enabled = true;
+        grabbed = true;
+        snapPos = _grabInfo.grabbedBy.transform;
+    }
+
+    void IsReleased()
+    {
+        seedParticles.SetActive(true);
+
+        StartCoroutine(ScaleDown());
+        flowers.SetActive(true);
+
+
+        //Destroy(gameObject, seedParticles.GetComponent<ParticleSystem>().main.startLifetime.constant);
+    }
+
+    private void FixedUpdate()
+    {
+        if (grabbed)
+        {
+            transform.position = snapPos.position;
+            transform.rotation = Quaternion.LookRotation(snapPos.forward);
+        }
+    }
+
+    IEnumerator ScaleDown( )
+    {
+        Transform t = transform;
+        GetComponent<SphereCollider>().enabled = false;
+
+        while (t.localScale.x <=.01f)
+        {
+            t.localScale *= .6f;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().useGravity = true;
+
+    }
+}
